40e6a3c9a35a487c1096108906dca4560767a522
WajidAhmed-25 <wajid.g20504@iqra.edu.pk>
2023-11-07 03:52:44 +0500

    first commit

---
+++ restapi-node-postgres/database/database.sql
@@ -0,0 +1,12 @@
+-- CREATE DATABASE firstapi;
+
+-- CREATE TABLE users(
+--     doc_id SERIAL PRIMARY KEY,
+--     doc_name TEXT,
+--     doc_email TEXT,
+--     doc_password TEXT
+-- );
+
+-- INSERT INTO doctors (doc_name, doc_email,doc_password) VALUES 
+-- ('mateus', 'mateus@api.com','waji'),
+-- ('joe', 'joe@api.com','lana');
